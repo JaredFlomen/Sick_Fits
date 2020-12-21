490cc1c7a40bc601793d6cecf34cbd0044876048
Jared Flomen <jaredflomen@gmail.com>
2020-12-21 13:58:03 -0500

    Using context API to wrap application

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
 
 const StyledPage = styled.div`
   background: white;
@@ -12,12 +22,12 @@ const Inner = styled.div`
   max-width: 1000px;
   margin: 0 auto;
   padding: 2rem;
-  background-color: red;
 `;
 
 class Page extends Component {
   render() {
     return (
+      <ThemeProvider theme={theme}> 
       <StyledPage>
         <Meta />
         <Header />
@@ -25,6 +35,7 @@ class Page extends Component {
           {this.props.children}
         </Inner>
       </StyledPage>
+      </ThemeProvider>
     );
   }
 }
