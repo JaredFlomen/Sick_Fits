a853036d982bbebba3f89f36bda9be53edcf9600
Jared Flomen <jaredflomen@gmail.com>
2020-12-25 14:56:09 -0500

    Currently logged in user with middleware and render props

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import User from './User';
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
@@ -19,6 +27,6 @@ const Nav = () => (
       <a>Account</a>
     </Link>
   </NavStyles>
-)
+);
 
 export default Nav;
